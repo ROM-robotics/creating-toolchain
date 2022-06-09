c16eaa6c5761f7fd57743ca24e355f9a7e082dce
Pyae Soan Aung <romrobotics@gmail.com>
2022-06-09 21:17:32 +0630

    07-if-foreach-while

---
+++ cmake-tutorial/07-if-foreach-while/build/CMakeFiles/hellobot.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mr_robot/Desktop/creating-toolchain/cmake-tutorial/07-if-foreach/src/main.cpp" "/home/mr_robot/Desktop/creating-toolchain/cmake-tutorial/07-if-foreach/build/CMakeFiles/hellobot.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-tutorial/07-if-foreach-while/build/CMakeFiles/hellobot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellobot.dir/src/main.cpp.o"
+  "hellobot"
+  "hellobot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
